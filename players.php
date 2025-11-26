49d83acd35d954778e66801e53fb415f16632b10
aswani26 <aswania9365@gmail.com>
2025-11-26 21:39:07 +0530

    players.php

--- players.php
+++ players.php
@@ -1,56 +1,65 @@
 <?php
-$players = array(
+
+$players = [
     "Rohit Sharma",
     "Virat Kohli",
-    "Jasprit Bumrah",
-    "Hardik Pandya",
     "Ravindra Jadeja",
-    "Shubman Gill",
-    "KL Rahul",
-    "Rishabhs Pant"
-);
+    "Jasprit Bumrah",
+    "KL Rahul"
+];
+
+$positions = [
+    "Batsman (Captain)",
+    "Batsman",
+    "All-Rounder",
+    "Bowler",
+    "Wicket Keeper/Batsman"
+];
+
 ?>
 <html>
 <head>
     <title>Indian Cricket Players</title>
     <style>
         table {
-            width: 40%;
+            width: 60%;
+            margin: auto;
             border-collapse: collapse;
-            margin: 20px auto;
         }
         th, td {
-            border: 1px solid ;
             padding: 10px;
+            border: 1px solid black;
             text-align: center;
         }
         th {
-            background-color:black;
+            background-color: #0275d8;
+            color: white;
         }
     </style>
 </head>
-
 <body>
 
-<h2 style="text-align:center;">List of Indian Cricket Players</h2>
+<h2 style="text-align:center; color:green;">Indian Cricket Players & Positions</h2>
 
 <table>
-<tr>
-<th>Sl. No</th>
-<th>Player Name</th>
-</tr>
-<?php
-$i = 1;
-foreach ($players as $player) {
-echo "<tr>
-<td>$i</td>
-<td>$player</td>
-</tr>";
-$i++;
-}
-?>
+    <tr>
+        <th>SI No.</th>
+        <th>Player Name</th>
+        <th>Position</th>
+    </tr>
+
+    <?php
+    for ($i = 0; $i < count($players); $i++) {
+        echo "<tr>";
+        echo "<td>" . ($i + 1) . "</td>";         
+        echo "<td>" . $players[$i] . "</td>";     
+        echo "<td>" . $positions[$i] . "</td>";  
+        echo "</tr>";
+    }
+    ?>
 </table>
 
 </body>
 </html>
+
 
